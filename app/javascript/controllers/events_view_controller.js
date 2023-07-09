dcbb481eca080aeae2c92f25898f842e361a3b12
Astrid Gealer <astrid@gealer.email>
2023-07-09 12:02:55 +0100

    rejig some things in prep for the homepage

---
+++ app/javascript/controllers/events_view_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import { geoIpResponse } from "globals"
+
+// Connects to data-controller="events-view"
+export default class extends Controller {
+  connect() {
+  }
+}
--- app/javascript/controllers/google_maps_controller.js
+++ app/javascript/controllers/google_maps_controller.js
@@ -1,24 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-
-// For the interests of transparency, this exists so that it is easy and extremely cheap to do geo-IP.
-// Here is the code I am using in that worker:
-// export default {
-//   async fetch(request, env, ctx) {
-//     return new Response(JSON.stringify({
-//       lat: request.cf.latitude, lng: request.cf.longitude,
-//     }), {
-//       headers: {
-//         'Content-Type': 'application/json; charset=utf-8',
-//         'Access-Control-Allow-Origin': '*',
-//       },
-//     })
-//   },
-// };
-const geoIpResponse = (async () => {
-  const res = await fetch("https://geo-ip.astrids.workers.dev")
-  if (res.status !== 200) throw new Error("Failed to fetch geo-ip")
-  return res.json()
-})()
+import { geoIpResponse } from "globals"
 
 // Connects to data-controller="google-maps"
 export default class extends Controller {
